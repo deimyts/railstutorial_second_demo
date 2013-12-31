f9bf5d2696c9908bcf410fd12e46c2413f4f6ede
Deimyts <camjonesdesign@gmail.com>
2013-12-30 18:20:34 -0600

    initial commit' git remote add origin https://github.com/deimyts/railstutorial_second_demo.git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '976c160be50c2d1fd125f7dfe6786203e0f99dd326e3d9913cc852ee5e57e27b64a1fe5eb5aabd6ba9cdf668317b7c778fef6f12e4c2755fd9eb3be3c091c219'
